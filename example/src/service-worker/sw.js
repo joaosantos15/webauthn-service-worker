c9a3487abc41469fc4dd81ab04537e6272222e3a
Joao Santos <jrmsantos15@gmail.com>
2019-03-14 15:02:11 +0000

    updated example

--- example/src/service-worker/sw.js
+++ example/src/service-worker/sw.js
@@ -1,17 +1,3 @@
-// const {RegisterHandler} = require('./handlers/register')
-// const {ResponseHandler} = require('./handlers/response')
-// const {LoginHandler} = require('./handlers/login')
-
-// const utils = require('./lib/utils')
-// const {IdbKeyVal} = require('./lib/idbKeyVal')
-// const {UserManager} = require('./lib/userManager')
-
-// const idbKeyval = new IdbKeyVal()
-// const userManager = new UserManager(idbKeyval)
-// const registerHandler = new RegisterHandler(idbKeyval, utils, userManager)
-// const loginHandler = new LoginHandler(idbKeyval, utils, userManager)
-// const responseHandler = new ResponseHandler(idbKeyval, utils, userManager)
-
 const webAuthnSw = require('../../../src/index')
 
 self.addEventListener('install', function (event) {
